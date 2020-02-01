fa3ec0cae22ff78534890c5abcd95ff828a74967
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2020-02-02 00:58:07 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 echo "hello Bangladesh";
+if(isset($_POST["submit"])){
+ $fp=fopen("data.txt", "a");
+  fwrite($fp, $_POST["name"];
+ fclose($fp);
+  echo "<font size='7' color='green'><br><br>saved</font>"
+}
 ?>
+<form method="POST">
+  <input name="name"/>
+  <input type="submit" name="submit" value="GO"/>
+ </form>
